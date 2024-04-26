04876db4f48c059aaa3c92507ca4c8f3c9150bf4
PVega17102 <is.paulvega.17102@gmail.com>
2024-04-26 01:48:21 -0600

    Update cors.js

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -2,6 +2,7 @@ import cors from 'cors'
 
 const ACCEPTED_ORIGINS = [
   'http://localhost:5173',
+  'http://localhost:3000',
   'https://artgora-admin.vercel.app',
   'https://artgora.vercel.app',
 ]
